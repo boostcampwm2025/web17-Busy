255c91e20aa0999898a56d757c19f9e5cee16603
seunghok22 <seunghok22@gmail.com>
2026-01-06 20:49:27 +0900

    chore: 컴포넌트 내부 common 경로 삭제 #44
    이미 컴포넌트 자체가 공용 경로이며 따로 분리가 필요하지 않다고 생각되어 수정하였습니다.

--- apps/web/src/components/common/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex h-full min-h-[200px] w-full items-center justify-center">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-gray-300 border-t-black" />
-    </div>
-  );
-}
