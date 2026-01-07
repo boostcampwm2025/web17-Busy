c85141836c84586de46644619a5a9be05a2ce21c
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2026-01-07 17:50:23 +0900

    feat: path alias 적용 #35

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as LoadingSpinner } from './LoadingSpinner';
 export { default as Sidebar } from './sidebar/Sidebar';
 export { default as RightPanel } from './player/RightPanel';
 export { default as ModalContainer } from './modals/ModalContainer';
+export { default as SearchDrawerContent } from './search/SearchDrawerContent';
