c964300efb31597d4b33c6f4a0692a06c8f49b01
Jae-Hyuk-Jang <lucentejjh@gmail.com>
2026-01-07 18:29:53 +0900

    chore: add barrel exports / 검색 배럴 익스포트 정리 #35

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,6 +1,6 @@
+export * from './player';
+export * from './sidebar';
+export * from './search';
 export { default as ErrorScreen } from './ErrorScreen';
 export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as Sidebar } from './sidebar/Sidebar';
-export { default as RightPanel } from './player/RightPanel';
 export { default as ModalContainer } from './modals/ModalContainer';
-export { default as SearchDrawerContent } from './search/SearchDrawerContent';
