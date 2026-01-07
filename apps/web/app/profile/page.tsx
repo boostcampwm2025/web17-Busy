87ee1beeb26baa824c39eff616fccdd0df554555
Yebin Kim <rladpqls07@gmail.com>
2026-01-07 21:19:25 +0900

    feat: 프로필 페이지 스켈레톤 UI 구현
    - 헤더를 포함한 기본 페이지 레이아웃 설정
    - next 앱 라우터 내장 로딩 UI를 활용하여 스켈레톤 파일 작성
    - 프로필 영역 레이아웃, 그리드 반응형 적용

--- apps/web/app/profile/page.tsx
+++ apps/web/app/profile/page.tsx
@@ -1,4 +1,5 @@
-export default function Profile() {
+export default async function Profile() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   return (
     <div className="flex h-full justify-center items-center">
       <p className="text-xl">프로필 페이지 영역</p>
