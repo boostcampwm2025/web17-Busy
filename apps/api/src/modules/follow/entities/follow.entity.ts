078adc57f9910e4f4bffbb8a94745056172cc3b4
Yechan Moon <moonyechan97@naver.com>
2026-01-11 01:04:37 +0900

    feat: follow 엔티티 클래스 작성

---
+++ apps/api/src/modules/follow/entities/follow.entity.ts
@@ -0,0 +1,29 @@
+import { User } from 'src/modules/user/entities/user.entity';
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Follow {
+  @PrimaryColumn('uuid', { name: 'following_user_id' })
+  followingUserId: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'following_user_id' })
+  followingUser: User;
+
+  @PrimaryColumn('uuid', { name: 'followed_user_id' })
+  followedUserId: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'followed_user_id' })
+  followedUser: User;
+
+  @CreateDateColumn()
+  createAt: Date;
+}
