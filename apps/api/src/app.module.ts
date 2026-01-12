173a229aefdfc0be0504c6a78e8b98d79f9c2631
Yechan Moon <moonyechan97@naver.com>
2026-01-12 16:48:30 +0900

    chore: typeorm 설정

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -12,6 +15,20 @@ import { UserModule } from './modules/user/user.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('HOST'),
+        port: config.get<number>('PORT', 3306),
+        username: config.get<string>('USERNAME'),
+        password: config.get<string>('PASSWORD'),
+        database: config.get<string>('DATABASE'),
+        entities: [join(__dirname, '**/*.entity.{ts,js}')],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
