29a330b95e5e31f8ea68586e1b499f7b9650d875
Yechan Moon <moonyechan97@naver.com>
2026-01-13 16:37:12 +0900

    fix: 비로그인 유저인 경우 userId가 null이 나오도록 수정

--- apps/api/src/common/decorators/userId.decorator.ts
+++ apps/api/src/common/decorators/userId.decorator.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const UserId = createParamDecorator<never, string>(
+export const UserId = createParamDecorator<never, string | null>(
   (_: never, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
     const user = req.user;
-    return user.sub ?? user.id;
+    return user?.sub ?? user?.id ?? null;
   },
 );
