4180a627f2a875e782ca136c7ca7362e1dc4e24b
Yechan Moon <moonyechan97@naver.com>
2026-01-13 18:31:07 +0900

    feat: post controller - AuthGuard 추가, @UserId 데코레이터 사용, try - catch 구조로 변경, 서비스 함수 호출, 리턴 타입 표시

--- apps/api/src/modules/post/post.controller.ts
+++ apps/api/src/modules/post/post.controller.ts
@@ -9,6 +9,8 @@ import {
   Param,
   ParseIntPipe,
   ParseUUIDPipe,
+  UseGuards,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { PostService } from './post.service';
 import {
@@ -19,58 +21,95 @@ import {
   FeedResponseDto,
   GetPostDetailResponseDto,
 } from '@repo/dto/post/res/index';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { UserId } from 'src/common/decorators/userId.decorator';
+import { FeedService } from './feed.service';
 
 @Controller('post')
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+  constructor(
+    private readonly postService: PostService,
+    private readonly feedservice: FeedService,
+  ) {}
 
   // feed 모둘이나 서비스 분리
   @Get('feed')
-  feed(
+  async feed(
+    @UserId() requestUserId: string | null,
     @Query('limit', ParseIntPipe) limit: number,
-    @Query('cursor', new ParseUUIDPipe({ version: '7' })) cursor: string,
-  ): FeedResponseDto {
-    return {
-      hasNest: false,
-      nextCursor: '111',
-      posts: [],
-    };
+    @Query('cursor', new ParseUUIDPipe({ version: '7' })) cursor?: string,
+  ): Promise<FeedResponseDto> {
+    try {
+      return await this.feedservice.feed(requestUserId, limit, cursor);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `피드 데이터를 불러오는 데 실패했습니다. 에러 메시지: ${error.message}`,
+      );
+    }
   }
 
+  @UseGuards(AuthGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostRequestDto) {
-    return { ok: true };
+  async create(
+    @UserId() requestUserId: string,
+    @Body() createPostDto: CreatePostRequestDto,
+  ): Promise<{ ok: true }> {
+    try {
+      const { musics, content, thumbnailImgUrl } = createPostDto;
+      await this.postService.create(
+        requestUserId,
+        musics,
+        content,
+        thumbnailImgUrl,
+      );
+      return { ok: true };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `피드 데이터를 불러오는 데 실패했습니다. 에러 메시지: ${error.message}`,
+      );
+    }
   }
 
   @Get(':id')
-  findById(@Param('id', ParseIntPipe) id: number): GetPostDetailResponseDto {
-    return {
-      postId: '111',
-      author: {
-        userId: '1',
-        nickname: 'chnn',
-      },
-      content: '노래 좋다',
-      thumbnailImgUrl: '',
-      musics: [],
-      likeCount: 0,
-      commentCount: 0,
-      isLiked: false,
-      createAt: new Date(),
-      isEdited: false,
-    };
+  async getPostDetail(
+    @UserId() requestUserId: string,
+    @Param('id') postId: string,
+  ): Promise<GetPostDetailResponseDto | null> {
+    try {
+      return await this.postService.getPostDetail(postId, requestUserId);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `피드 데이터를 불러오는 데 실패했습니다. 에러 메시지: ${error.message}`,
+      );
+    }
   }
 
   @Patch(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
+  async update(
+    @Param('id', ParseIntPipe) postId: string,
     @Body() updatePostDto: UpdatePostRequestDto,
-  ) {
-    return { ok: true };
+  ): Promise<{ ok: true }> {
+    try {
+      this.postService.update(postId, updatePostDto.content);
+      return { ok: true };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `피드 데이터를 불러오는 데 실패했습니다. 에러 메시지: ${error.message}`,
+      );
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id', ParseIntPipe) id: number) {
-    return { ok: true };
+  async delete(
+    @Param('id', ParseIntPipe) postId: string,
+  ): Promise<{ ok: true }> {
+    try {
+      await this.postService.delete(postId);
+      return { ok: true };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        `피드 데이터를 불러오는 데 실패했습니다. 에러 메시지: ${error.message}`,
+      );
+    }
   }
 }
