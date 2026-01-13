19642472ef0a445d8862334181aac5b05f8d7426
Yechan Moon <moonyechan97@naver.com>
2026-01-13 18:27:44 +0900

    feat: post 모듈에 PostMusic, Like 레포 import, FeedService 추가

--- apps/api/src/modules/post/post.module.ts
+++ apps/api/src/modules/post/post.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, Post } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedService } from './feed.service';
+import { Like } from '../like/entities/like.entity';
+import { PostMusic } from './entities/post-music.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Post, PostMusic, Like])],
   controllers: [PostController],
-  providers: [PostService],
+  providers: [PostService, FeedService],
 })
 export class PostModule {}
