b5542fe71bb74c6637b52613bc56281ab1347011
seunghok22 <seunghok22@gmail.com>
2026-01-13 17:28:58 +0900

    feat: 플레이리스트 업데이트 api 구현 및 설정 #78

--- apps/api/src/modules/now-playlist/now-playlist.service.ts
+++ apps/api/src/modules/now-playlist/now-playlist.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateNowPlaylistReqDto } from '@repo/dto/now-playlist/req/update-now-playlist.dto';
+import { NowPlaylistRepository } from './now-playlist.repository';
 
 @Injectable()
-export class NowPlaylistService {}
+export class NowPlaylistService {
+  constructor(private readonly nowPlaylistRepository: NowPlaylistRepository) {}
+
+  async updateNowPlaylist(
+    userId: string,
+    dto: UpdateNowPlaylistReqDto,
+  ): Promise<void> {
+    await this.nowPlaylistRepository.refreshPlaylist(userId, dto.musicIds);
+  }
+}
