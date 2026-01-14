03b7b05ca1968faae1be79440ee8b559843cb217
Yechan Moon <moonyechan97@naver.com>
2026-01-14 17:32:46 +0900

    feat: 알림 시드데이터 주입 로직 추가

--- apps/api/src/modules/seed/seed.module.ts
+++ apps/api/src/modules/seed/seed.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { DevSeedService } from './dev-seed.service';
+import { Noti } from '../noti/entities/noti.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Noti])],
   providers: [DevSeedService],
 })
 export class SeedModule {}
