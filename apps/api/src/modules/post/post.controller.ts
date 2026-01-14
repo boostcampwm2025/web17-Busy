c7cbaaa58d7b94316f3d2c80dc976b5f9f5c1bc1
Yebin Kim <rladpqls07@gmail.com>
2026-01-14 20:24:36 +0900

    feat: cursor 파라미터 검사 옵션 추가
    - 파라미터 값이 optional함을 파이프 설정에 추가

--- apps/api/src/modules/post/post.controller.ts
+++ apps/api/src/modules/post/post.controller.ts
@@ -35,11 +35,11 @@ export class PostController {
   async feed(
     @UserId() requestUserId: string | null,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
-    @Query('cursor', new ParseUUIDPipe({ version: '7' })) cursor?: string,
+    @Query('cursor', new ParseUUIDPipe({ version: '7', optional: true }))
+    cursor?: string,
   ): Promise<FeedResponseDto> {
     try {
-      return { posts: [], hasNext: false, nextCursor: undefined };
-      //return await this.feedservice.getFeedPosts(requestUserId, limit, cursor);
+      return await this.feedservice.getFeedPosts(requestUserId, limit, cursor);
     } catch (error) {
       throw new InternalServerErrorException(
         `피드 데이터를 불러오는 데 실패했습니다. 에러 메시지: ${error.message}`,
