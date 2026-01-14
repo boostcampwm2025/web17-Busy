53a530ee4edd74a4ec614f297e2db67d05c2b8ba
Yechan Moon <moonyechan97@naver.com>
2026-01-15 01:00:14 +0900

    feat: 이미지 포함한 게시글 생성 요청 dto 새로 작성

--- packages/dto/src/post/req/index.ts
+++ packages/dto/src/post/req/index.ts
@@ -1,3 +1,4 @@
 export { CreatePostRequestDto } from './createPostRequestDto';
 export type { MusicRequest } from './createPostRequestDto';
 export { UpdatePostRequestDto } from './updatePostRequestDto';
+export { CreatePostMultipartDto } from './createPostMultipartDto';
