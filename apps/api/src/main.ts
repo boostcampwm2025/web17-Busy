a3cab9a1d75f1f577657a0d421f267fb03486366
seunghok22 <seunghok22@gmail.com>
2026-01-15 22:22:56 +0900

    fix: be 포트 설정 변경 3002 -> 3000

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(process.env.PORT ?? 3002);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
