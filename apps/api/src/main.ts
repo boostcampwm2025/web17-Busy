d573fd12bd524ee31eb2274ee17954c6ac06623e
seunghok22 <seunghok22@gmail.com>
2026-01-15 23:45:20 +0900

    fix: be api 프리픽스 적용

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
