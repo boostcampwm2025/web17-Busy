1030ed7da15c1f2e931bd212a187464892621dc3
seunghok22 <seunghok22@gmail.com>
2026-01-16 00:34:16 +0900

    chore: be main.ts에 배포환경 cors 설정

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,10 @@ import { AllExceptionsFilter } from './common/filter/all-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: ['https://vibr.site', 'http://localhost:3001'],
+    credentials: true,
+  });
   app.useGlobalFilters(new AllExceptionsFilter());
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
