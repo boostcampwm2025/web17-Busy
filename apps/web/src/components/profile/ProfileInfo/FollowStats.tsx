e2f717aa6ea79095d5d9fb2c11e4b5f38c853f6a
Yebin Kim <rladpqls07@gmail.com>
2026-01-20 23:25:52 +0900

    feat: 팔로우 숫자 포맷팅 유틸 함수 구현
    - 1000 미만은 그대로 출력
    - 천 이상은 'K' 단위로 출력
    - 백만 이상은 'M' 단위로 출력
    - 소수점 1자리까지 출력, 정수는 소수점 제거

--- apps/web/src/components/profile/ProfileInfo/FollowStats.tsx
+++ apps/web/src/components/profile/ProfileInfo/FollowStats.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MODAL_TYPES, useModalStore } from '@/stores';
+import { formatFollowCount } from '@/utils';
 
 interface FollowStatsProps {
   profileUserId: string;
@@ -14,16 +15,15 @@ export default function FollowStats({ profileUserId, followerCount, followingCou
   const handleOpenFollowerModal = () => openModal(MODAL_TYPES.FOLLOWER_USER, { profileUserId });
   const handleOpenFollowingModal = () => openModal(MODAL_TYPES.FOLLOWING_USER, { profileUserId });
 
-  // TODO: 팔로워/팔로잉 숫자 포맷팅 함수
   return (
     <div className="flex items-center justify-center md:justify-start space-x-6 mb-4 text-primary">
       <button onClick={handleOpenFollowerModal} title="팔로워 목록" className="flex flex-col-reverse items-center md:flex-row ">
         <span className="text-sm font-medium text-gray-1">팔로워</span>
-        <span className="font-bold text-xl md:ml-2">{followerCount}</span>
+        <span className="font-bold text-xl md:ml-2">{formatFollowCount(followerCount)}</span>
       </button>
       <button onClick={handleOpenFollowingModal} title="팔로잉 목록" className="flex flex-col-reverse items-center md:flex-row ">
         <span className="text-sm font-medium text-gray-1">팔로잉</span>
-        <span className="font-bold text-xl md:ml-2">{followingCount}</span>
+        <span className="font-bold text-xl md:ml-2">{formatFollowCount(followingCount)}</span>
       </button>
     </div>
   );
