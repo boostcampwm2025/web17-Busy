2f4e038fd07e48730dd02c48987c163beea593d2
Yechan Moon <moonyechan97@naver.com>
2026-01-21 17:56:44 +0900

    feat: playlist dto에 class-validator 데코레이터 추가

--- packages/dto/src/playlist/req/add-music-to-playlist.dto.ts
+++ packages/dto/src/playlist/req/add-music-to-playlist.dto.ts
@@ -1,5 +1,10 @@
+import { Type } from 'class-transformer';
+import { IsArray, ValidateNested } from 'class-validator';
 import { MusicRequestDto } from '../../music';
 
 export class AddMusicToPlaylistReqDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => MusicRequestDto)
   musics: MusicRequestDto[];
 }
--- packages/dto/src/playlist/req/update-musics-order-of-playlist.dto.ts
+++ packages/dto/src/playlist/req/update-musics-order-of-playlist.dto.ts
@@ -1,3 +1,7 @@
+import { ArrayNotEmpty, IsArray, IsUUID } from 'class-validator';
+
 export class UpdateMusicsOrderOfPlaylistReqDto {
+  @IsArray()
+  @IsUUID('7', { each: true })
   musicIds: string[];
 }
