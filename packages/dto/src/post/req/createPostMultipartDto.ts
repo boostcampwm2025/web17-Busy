298b640a72257085e4f8bdf4b63bc16a2cbf760f
Yechan Moon <moonyechan97@naver.com>
2026-01-21 18:10:45 +0900

    feat: post dto 에 class validator 데코레이터 작성
    - post 생성 시 contents는 옵셔널로 수정 (post service)

--- packages/dto/src/post/req/createPostMultipartDto.ts
+++ packages/dto/src/post/req/createPostMultipartDto.ts
@@ -1,5 +1,13 @@
+import { IsJSON, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreatePostMultipartDto {
-  content: string;
+  @IsOptional()
+  @IsString()
+  content?: string;
+
   // musics: MusicRequest[] 를 "문자열"로 받는다 (JSON string)
+  @IsString()
+  @IsNotEmpty()
+  @IsJSON()
   musics: string;
 }
