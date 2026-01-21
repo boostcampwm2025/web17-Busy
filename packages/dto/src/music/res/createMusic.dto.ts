8ab268fb8bae1eb25333e115d7def9162aa6e379
Yechan Moon <moonyechan97@naver.com>
2026-01-21 17:51:17 +0900

    feat: music dto에 class-validator 데코레이터 추가

--- packages/dto/src/music/res/createMusic.dto.ts
+++ packages/dto/src/music/res/createMusic.dto.ts
@@ -1,12 +1,3 @@
-import { MusicProvider } from '../music-provider.enum';
-import { MusicRequestDto } from '../music.dto';
+import { MusicResponseDto } from '../music.dto';
 
-export class CreateMusicResDto implements MusicRequestDto {
-  id: string;
-  trackUri: string;
-  provider: MusicProvider;
-  albumCoverUrl: string;
-  title: string;
-  artistName: string;
-  durationMs: number;
-}
+export class CreateMusicResDto extends MusicResponseDto {}
