cfbbb0b916f1aa92deda907d12765b0b06ce1c28
Yechan Moon <moonyechan97@naver.com>
2026-01-21 17:02:30 +0900

    chore: 기존에 dto 파일에 쓰던 class-validator의 데코레이터들에 타입 오류나던거 해결
    - tsconfig.json 수정
    - 백엔드에서 global pipe 쓰도록 설정(main.ts)
    - 백엔드에서 class-transformer, class-validator 의존성 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { AllExceptionsFilter } from './common/filter/all-exception.filter';
@@ -6,6 +7,13 @@ import { AllExceptionsFilter } from './common/filter/all-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.enableCors({
     origin: ['https://vibr.site', 'http://localhost:3001'],
     credentials: true,
