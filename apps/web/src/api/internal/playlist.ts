aff382b497c1eb8f20b0329b61ff4844d944cf8a
Yechan Moon <moonyechan97@naver.com>
2026-01-22 01:24:25 +0900

    feat: 플리 api 요청 함수 정리

--- apps/web/src/api/internal/playlist.ts
+++ apps/web/src/api/internal/playlist.ts
@@ -1,5 +1,5 @@
 import { internalClient } from './client';
-import type { GetAllPlaylistsResDto, GetPlaylistDetailResDto } from '@repo/dto';
+import type { CreatePlaylistResDto, GetAllPlaylistsResDto, GetPlaylistDetailResDto, MusicRequestDto } from '@repo/dto';
 
 // 보관함 목록 가져오기 (미리보기)
 export const getAllPlaylists = async (): Promise<GetAllPlaylistsResDto['playlists']> => {
@@ -10,5 +10,35 @@ export const getAllPlaylists = async (): Promise<GetAllPlaylistsResDto['playlist
 // 보관함 플레이리스트 하나 자세한 정보 가져오기 (상세보기)
 export const getPlaylistDetail = async (playlistId: string): Promise<GetPlaylistDetailResDto> => {
   const { data } = await internalClient.get<GetPlaylistDetailResDto>(`/playlist/${playlistId}`);
+  return data;
+};
+
+// 새로운 플리 추가
+export const createNewPlaylist = async (): Promise<CreatePlaylistResDto> => {
+  const { data } = await internalClient.post<CreatePlaylistResDto>('/playlist');
+  return data;
+};
+
+// 플리 제목 수정
+export const editTitleOfPlaylist = async (playlistId: string, title: string): Promise<{ ok: true }> => {
+  const { data } = await internalClient.patch<{ ok: true }>(`/playlist/${playlistId}`, { title });
+  return data;
+};
+
+// 플리 삭제
+export const deletePlaylist = async (playlistId: string): Promise<{ ok: true }> => {
+  const { data } = await internalClient.delete<{ ok: true }>(`/playlist/${playlistId}`);
+  return data;
+};
+
+// 플리에 음악 추가
+export const addMusicsToPlaylist = async (playlistId: string, musics: MusicRequestDto): Promise<GetPlaylistDetailResDto> => {
+  const { data } = await internalClient.post<GetPlaylistDetailResDto>(`/playlist/${playlistId}/music`, { musics });
+  return data;
+};
+
+// 플리 음악 순서 변경
+export const changeMusicOrderOfPlaylsit = async (playlistId: string, musicIds: string[]): Promise<{ ok: true }> => {
+  const { data } = await internalClient.put<{ ok: true }>(`/playlist/${playlistId}/music`, { musicIds });
   return data;
 };
