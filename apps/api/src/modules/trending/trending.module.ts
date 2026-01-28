000de483db61944f5704ebd3185b7a1448b57f81
Yechan Moon <moonyechan97@naver.com>
2026-01-29 01:01:23 +0900

    chore: 기존 테스트를 위해 작성한 trending controller, trending.http 삭제

--- apps/api/src/modules/trending/trending.module.ts
+++ apps/api/src/modules/trending/trending.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RedisHealthService } from './redis-health.service';
-import { TrendingController } from './trending.controller';
 import { TrendingService } from './trending.service';
 
 @Module({
   providers: [RedisHealthService, TrendingService],
-  controllers: [TrendingController],
   exports: [TrendingService],
 })
 export class TrendingModule {}
